6a54bee52a7dd9fc429426b3934c78c4b450e841
Jori Regter <joriregter@gmail.com>
2017-03-11 12:10:43 +0100

    Changes project structure to support react from root

---
+++ client/app/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import HelloWorld from './container/HelloWorld';
+
+const App = (props) => (
+  <HelloWorld {...props} />
+);
+
+export default App;
---
+++ client/app/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactOnRails from 'react-on-rails';
+
+import App from './components/App';
+
+// This is how react_on_rails can see the HelloWorldApp in the browser.
+ReactOnRails.register({ App });
