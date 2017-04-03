5feede5a6132facfc61906a57a3d027e6095c9da
Jori Regter <joriregter@gmail.com>
2017-04-03 15:23:11 +0200

    Adds models

--- client/app/components/presentational/ExperienceScreen.jsx
+++ client/app/components/presentational/ExperienceScreen.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import 'aframe';
-import {Scene, Entity} from 'aframe-react';
-import tableAndChair from '../../models/tableAndChair.dae'
 import laminate from '../../models/laminate.jpg'
-import obj from '../../models/interior.obj';
-import mtl from '../../models/interior.mtl';
+import model from '../../models/faume_test_alleen_bank.dae';
 
 const ExperienceScreen = () => {
   return (
     <a-scene vr-mode-ui="enabled: true">
       <a-assets>
-       <a-asset-item id="tree-obj" src={obj}></a-asset-item>
-       <a-asset-item id="tree-mtl" src={mtl}></a-asset-item>
-     </a-assets>
+        <img src={laminate} id="laminate" />
+        <a-asset-item id="model" src={model}></a-asset-item>
+        <img id="skyTexture"
+          src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg" />
+      </a-assets>
 
-     <a-sky></a-sky>
-
-     <a-camera userHeight="2">
-       <a-cursor color="#ffffff"></a-cursor>
-     </a-camera>
+      <a-sky src="#skyTexture"></a-sky>
+      <a-collada-model src="#model"></a-collada-model>
+      <a-plane src="#laminate" height="4.55" width="6" position="2.64 0 0.22" rotation="-90 0 0"></a-plane>
+      <a-camera userHeight="2">
+        <a-cursor color="black"></a-cursor>
+      </a-camera>
    </a-scene>
   )
 }
