de343d1cf01d8c5bd7b4f41376430fb2f7b1d7e1
Jonathan Emanuels <j.emanuels@outlook.com>
2017-04-04 10:56:26 +0200

    Experience link classes gegeven

--- client/app/components/presentational/UsageStepNavigation.jsx
+++ client/app/components/presentational/UsageStepNavigation.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 const UsageStepNavigation = ({ currentStep, totalSteps, setStep }) => {
   const previousButton = (currentStep !== 0) ? <a className="previous" href="#" onClick={(e) => { e.preventDefault(); setStep(currentStep - 1); }}><section className="button">Previous</section></a> : null;
-    const nextButton = (totalSteps - 1 > currentStep) ? <a className="next" href="#" onClick={(e) => { e.preventDefault(); setStep(currentStep + 1); }}><section className="button">Next</section></a> : ((currentStep === totalSteps - 1) ? <Link to="/experience">Experience</Link> : null);
+    const nextButton = (totalSteps - 1 > currentStep) ? <a className="next" href="#" onClick={(e) => { e.preventDefault(); setStep(currentStep + 1); }}><section
+            className="button">Next</section></a> : ((currentStep === totalSteps - 1) ? <Link className="next" to="/experience"><section className="button">Experience</section></Link> : null);
 
   return (
     <div className="nextAndPrevious">
