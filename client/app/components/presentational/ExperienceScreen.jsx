c92fefa70261960aadbcd9cba03588f8cbab6983
Arjo Bruijnes <arjo@weirdwater.net>
2017-04-04 11:23:08 +0200

    Add wallpaper to interior remove unused textures

--- client/app/components/presentational/ExperienceScreen.jsx
+++ client/app/components/presentational/ExperienceScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'aframe';
 import laminate from '../../models/laminate.jpg'
+import wallpaper from '../../models/floral-retro-70s-wallpaper.jpg'
 import model from '../../models/faume.dae';
 
 const ExperienceScreen = () => {
@@ -8,6 +9,7 @@ const ExperienceScreen = () => {
     <a-scene vr-mode-ui="enabled: true">
       <a-assets>
         <img src={laminate} id="laminate" />
+        <img src={wallpaper} id="wallpaper"
         <a-asset-item id="model" src={model}></a-asset-item>
         <img id="skyTexture"
           src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg" />
@@ -19,7 +21,7 @@ const ExperienceScreen = () => {
       <a-entity position="5.60 1.33 0.22" rotation="0 -90 0" geometry="mergeTo:null;primitive:plane;height:2.66;width:6" material="normalScale:{&quot;x&quot;:1,&quot;y&quot;:1}"></a-entity>
       <a-entity position="2.64 2.64 0.22" rotation="90 0 0" geometry="mergeTo:null;primitive:plane;height:4.55;width:6" material="normalScale:{&quot;x&quot;:1,&quot;y&quot;:1}"></a-entity>
       <a-entity position="2.64 1.33 -2.055" rotation="0 0 0" geometry="mergeTo:null;primitive:plane;height:2.66;width:6" material="normalScale:{&quot;x&quot;:1,&quot;y&quot;:1}"></a-entity>
-      <a-entity position="2.64 1.33 2.495" rotation="180 0 0" geometry="mergeTo:null;primitive:plane;height:2.66;width:6" material="normalScale:{&quot;x&quot;:1,&quot;y&quot;:1}"></a-entity>
+      <a-entity src="#wallpaper" position="2.64 1.33 2.495" rotation="180 0 0" geometry="mergeTo:null;primitive:plane;height:2.66;width:6" material="normalScale:{&quot;x&quot;:1,&quot;y&quot;:1}"></a-entity>
       <a-camera userHeight="2">
         <a-cursor color="black"></a-cursor>
       </a-camera>
